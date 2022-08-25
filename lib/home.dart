28026bfa32e207e7e8345bdaf0f3b74a1cd01c55
Diyorbek0617 <nizomiddinov.d.d@gmail.com>
2022-08-25 16:38:02 +0500

    changed

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
 import 'package:wifi_internet_controll/shows.dart';
@@ -6,14 +8,31 @@ import 'package:wifi_internet_controll/utills.dart';
 class Home extends StatefulWidget {
   final String title;
   final Color color;
-  const Home({required this.title,required this.color});
-  static const String id="Home";
+   const Home({Key? key,required this.title, required this.color}): super(key: key);
+  static const String id = "Home";
 
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  StreamSubscription? subscription;
+
+  @override
+  void initState() {
+    subscription =
+        Connectivity().onConnectivityChanged.listen(showConnetivitySnackBar);
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+
+    subscription!.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,12 +44,15 @@ class _HomeState extends State<Home> {
       ),
       body: Center(
         child: ElevatedButton(
-          onPressed: ()async{
-            final result=await Connectivity().checkConnectivity();
+          onPressed: () async {
+            final result = await Connectivity().checkConnectivity();
             showConnetivitySnackBar(result);
           },
           style: ElevatedButton.styleFrom(padding: const EdgeInsets.all(12)),
-          child: const Text("Check Connection",style: TextStyle(fontSize: 20.0),),
+          child: const Text(
+            "Check Connection",
+            style: TextStyle(fontSize: 20.0),
+          ),
         ),
       ),
     );
@@ -38,10 +60,11 @@ class _HomeState extends State<Home> {
 
   void showConnetivitySnackBar(ConnectivityResult result) {
     final hasInternet = result != ConnectivityResult.none;
-    final message = hasInternet?"You have again ${result.toString()}":
-        "You have no Internet";
-    final color = hasInternet?Colors.blue:Colors.red;
+    final message = hasInternet
+        ? "You have again ${result.toString()}"
+        : "You have no Internet";
+    final color = hasInternet ? Colors.blue : Colors.red;
     Utils.showTopSnackBar(context, message, color);
-    fireToast(context,"by Nizomiddinov",color);
+    fireToast(context, "by Nizomiddinov", color);
   }
 }
